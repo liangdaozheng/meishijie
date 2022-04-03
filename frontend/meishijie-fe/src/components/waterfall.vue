3001d1bb8d09f1121242ac3ab11408581c0e6b6f
liangdaozheng <l_daozheng@163.com>
2022-04-03 21:28:50 +0800

    登录

--- frontend/meishijie-fe/src/components/waterfall.vue
+++ frontend/meishijie-fe/src/components/waterfall.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="waterfall" ref="waterfall">
     <slot></slot>
-    <div class="waterfall-loading" ref='loading' v-show="isLoading">
+    <div class="waterfall-loading" ref="loading" v-show="isLoading">
       <i class="el-icon-loading"></i>
     </div>
   </div>
@@ -9,39 +9,41 @@
 <script>
 // 什么时候到可视区中了
 // waterfall 元素的下边距 < 可视区的高度 到达可视区
-import {throttle} from 'throttle-debounce'
+import { throttle } from "throttle-debounce";
 export default {
-  name: 'Waterfall',
-  data(){
+  name: "Waterfall",
+  data() {
     return {
-      isLoading: false
-    }
+      isLoading: false,
+    };
   },
-  mounted(){
+  mounted() {
     // 优化，每隔一段时间再去执行函数，不用频繁触发  节流函数
-
     this.scrollHandler = throttle(300, this.scroll.bind(this));
-    window.addEventListener('scroll', this.scrollHandler);
+    window.addEventListener("scroll", this.scrollHandler);
   },
-  destroyed(){
-    window.removeEventListener('scroll', this.scrollHandler)
+  destroyed() {
+    window.removeEventListener("scroll", this.scrollHandler);
   },
-  methods:{
-    scroll(){
-      console.log(123)
-      if(this.isLoading) return;
-      if(this.$refs.waterfall.getBoundingClientRect().bottom < document.documentElement.clientHeight){
-        console.log('已到达可视区')
+  methods: {
+    scroll() {
+      console.log(123);
+      if (this.isLoading) return;
+      if (
+        this.$refs.waterfall.getBoundingClientRect().bottom <
+        document.documentElement.clientHeight
+      ) {
         this.isLoading = true;
-        this.$emit('view')
+        this.$emit("view");
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
 <style lang="stylus">
-.waterfall-loading
-  width 100%
-  height 20px
-  text-align center
+.waterfall-loading {
+  width: 100%;
+  height: 20px;
+  text-align: center;
+}
 </style>
--- frontend/meishijie-fe/src/views/user-login/login.vue
+++ frontend/meishijie-fe/src/views/user-login/login.vue
@@ -1,75 +1,84 @@
 <template>
   <div class="login-section">
-    <el-form 
+    <el-form
       label-position="top"
       :rules="rules"
-      :model="ruleForm" status-icon 
-       ref="ruleForm" label-width="100px" class="demo-ruleForm"
+      :model="ruleForm"
+      status-icon
+      ref="ruleForm"
+      label-width="100px"
+      class="demo-ruleForm"
     >
       <el-form-item label="用户名" prop="name">
-        <el-input type="text" v-model="ruleForm.name" autocomplete="off"></el-input>
+        <el-input
+          type="text"
+          v-model="ruleForm.name"
+          autocomplete="off"
+        ></el-input>
       </el-form-item>
       <el-form-item label="密码" prop="password">
-        <el-input type="password" v-model="ruleForm.password" autocomplete="off"></el-input>
+        <el-input
+          type="password"
+          v-model="ruleForm.password"
+          autocomplete="off"
+        ></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+        <el-button type="primary" @click="submitForm('ruleForm')"
+          >提交</el-button
+        >
         <el-button @click="resetForm('ruleForm')">重置</el-button>
       </el-form-item>
     </el-form>
   </div>
 </template>
 <script>
-import {login} from '@/service/api';
+import { login } from "@/service/api";
 
 export default {
   data() {
     return {
       ruleForm: {
-        name: '',
-        password: ''
+        name: "",
+        password: "",
       },
       rules: {
-        name: [
-          { required: true, message: '请输入活动名称', trigger: 'blur' }
-        ],
-        password: [
-          { required: true, message: '请输入密码', trigger: 'blur' }
-        ]
-      }
+        name: [{ required: true, message: "请输入活动名称", trigger: "blur" }],
+        password: [{ required: true, message: "请输入密码", trigger: "blur" }],
+      },
     };
   },
   methods: {
     submitForm(formName) {
       this.$refs[formName].validate(async (valid) => {
-
         if (valid) {
           login({
             name: this.ruleForm.name,
-            password: this.ruleForm.password
+            password: this.ruleForm.password,
           }).then((data) => {
-            if(data.code === 0) {  
-              localStorage.setItem('token', data.data.token);
-              window.location.href = '/';
+            if (data.code === 0) {
+              localStorage.setItem("token", data.data.token);
+              window.location.href = "/";
             }
-            if(data.code === 1){
+            if (data.code === 1) {
               this.$message.error(data.mes);
             }
-          })
+          });
         } else {
-          console.log('error submit!!');
+          console.log("error submit!!");
           return false;
         }
       });
     },
     resetForm(formName) {
       this.$refs[formName].resetFields();
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style lang="stylus">
-.login-section
-  padding 0px 20px
+.login-section {
+  padding: 0px 20px;
+}
 </style>
